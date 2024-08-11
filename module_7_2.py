5d7bcf51814c3caf05c49fbf6a036a930a9c4295
Nikita <niknakels@gmail.com>
2024-08-11 21:54:45 +1100

    module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -1,28 +1,27 @@
 def custom_write(file_name, strings: list):
-  file = open(f'{file_name}', 'w', encoding='utf-8')
-  string = int()
-  crs = int()
-  keylist = []
-  valuelist = []
-  for i in strings:
-    string = 1 + strings.index(i)
-    crs = file.tell()
-    file.write(f'{i}\n')
+    file = open(f'{file_name}', 'w', encoding='utf-8')
+    string = int()
+    crs = int()
+    keylist = []
+    valuelist = []
+    for i in strings:
+        string = 1 + strings.index(i)
+        crs = file.tell()
+        file.write(f'{i} \n')
+        keylist.append((string, crs))
+        valuelist.append(i)
+    strings_positions = dict(zip(keylist, valuelist))
     file.close()
-    keylist.append((string, crs))
-    valuelist.append(i)
-  strings_positions = dict(zip(keylist, valuelist))
-  return strings_positions
+    return strings_positions
 
 
 info = [
-  'Text for tell.',
-  'Используйте кодировку utf-8.',
-  'Because there are 2 languages!',
-  'Спасибо!'
-  ]
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
 
 result = custom_write('test.txt', info)
 for elem in result.items():
-  print(elem)
-  
+    print(elem)
